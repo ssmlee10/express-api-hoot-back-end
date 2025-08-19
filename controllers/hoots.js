1d0010ded2eb0076978d475ae6e888e3dadd3b9b
Sharon Lee <ssmlee10@gmail.com>
2025-08-19 10:15:50 -0700

    hoot show functionality: COMPLETE

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -27,4 +27,14 @@ router.get('/', verifyToken, async (req, res) => {
   }
 });
 
+// GET  /hoots/:hootId
+router.get('/:hootId', verifyToken, async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId).populate('author');
+        res.status(200).json(hoot);
+        } catch(err) {
+        res.status(500).json({ err: err.message });
+    }
+})
+
 module.exports = router;
