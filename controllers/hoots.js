f2cacc84402cb5c03626590c5d32b16b3224b463
Sharon Lee <ssmlee10@gmail.com>
2025-08-19 12:50:20 -0700

    hoot udpdate comment functionality: COMPLETE

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -75,6 +75,7 @@ router.delete("/:hootId", verifyToken, async (req, res) => {
   }
 });
 
+// COMMENTS
 // POST  /hoots/:hootId/comments
 router.post("/:hootId/comments", verifyToken, async (req, res) => {
   try {
@@ -92,6 +93,28 @@ router.post("/:hootId/comments", verifyToken, async (req, res) => {
     //respond with the newComment
     res.status(201).json(newComment);
   } catch(err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+
+// PUT  hoots/:hootId/comments/:commentId
+router.put('/:hootId/comments/:commentId', verifyToken, async (req, res) => {
+   try {
+    const hoot = await Hoot.findById(req.params.hootId);
+    const comment = hoot.comments.id(req.params.commentId);
+
+    // ensures the current user is the author of the comment
+    if (comment.author.toString() !== req.user._id) {
+      return res
+        .status(403)
+        .json({ message: 'You are not authorized to edit this comment' });
+    }
+
+    comment.text = req.body.text;
+    await hoot.save();
+    res.status(200).json({ message: "Comment updated successfully" });
+  } catch (err) {
     res.status(500).json({ err: err.message });
   }
 });
